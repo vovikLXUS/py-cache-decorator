ef5e806d5c0a1e3cdb567a3594717fb87caf1930
Volodya Datsyshyn <imvovo4kaa@gmail.com>
2026-04-04 00:11:20 +0300

    added the solution of task named 'cache-decorator'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+    @functools.wraps(func)
+
+    def wrapper(*args, **kwargs) -> Callable:
+        if isinstance(kwargs.values(), (list, dict, set)):
+           return "Cannot work with mutable data types"
+        
+        key = (args, tuple(sorted(kwargs.items())))
+
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dict[key] = result
+        return result
+
+    return wrapper
